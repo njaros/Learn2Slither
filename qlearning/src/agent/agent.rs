f9792f476e16a52a04c35455fc3c3a66d7167e2a
Nicolas Jaros <njaros@z4r8p4.42lyon.fr>
2026-04-06 11:05:56 +0200

    remove train_loop duplicate

--- qlearning/src/agent/agent.rs
+++ qlearning/src/agent/agent.rs
@@ -1,12 +1,11 @@
 use convenient_lib::{Res, Void};
-use playground::{Dir, PlayGround};
+use playground::Dir;
 use rand::{RngExt, make_rng, rngs::StdRng, seq::IndexedRandom};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::fs;
 use std::io::{BufReader, BufWriter, Read};
 
-use crate::reward::reward_interpretor::RewardInterpretor;
 use crate::state::ETSFactory;
 use crate::state::ets_lib::ETS;
 
@@ -218,55 +217,4 @@ impl Agent {
             })
             .0]
     }
-}
-
-fn dir_to_usize(dir: Dir) -> usize {
-    match dir {
-        Dir::Up => 0,
-        Dir::Right => 1,
-        Dir::Down => 2,
-        Dir::Left => 3,
-    }
-}
-
-pub fn train_loop(agent: &mut Agent, rounds: usize, from_cli: bool) -> Void {
-    let mut reward_interpretor = RewardInterpretor::new();
-    let mut best_score = 0u32;
-
-    (1..rounds).for_each(|round| {
-        let mut playground: PlayGround = PlayGround::new(10, 10, make_rng());
-        let env = &playground.snake_view();
-        reward_interpretor.init(env);
-        let mut state = agent.ets.env_to_state(env);
-        let mut dir = agent.play(state);
-        let mut env = playground.next(dir);
-        while playground.is_alive() {
-            let next_state = agent.ets.env_to_state(&env);
-            let reward = reward_interpretor.get_reward(&env);
-            agent.bellman(state, Some(next_state), dir_to_usize(dir), reward);
-            state = next_state;
-            dir = agent.play(state);
-            env = playground.next(dir);
-        }
-        agent.bellman(
-            state,
-            None,
-            dir_to_usize(dir),
-            reward_interpretor.end_training_reward,
-        );
-        let score = playground.get_score();
-        if from_cli {
-            if score > best_score {
-                best_score = score;
-            }
-            println!("try: {round}: score: {score}, best: {best_score}");
-        }
-        agent.store_score(score);
-        agent.reduce_exploration_by(1. / (0.8 * rounds as f64));
-        agent.increase_discount_factor_by(0.75 / rounds as f64);
-    });
-
-    agent.save()?;
-
-    Ok(())
 }
